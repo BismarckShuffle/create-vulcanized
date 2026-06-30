308d3ea70b4c23d5df272fde094d4d1325ec471b
BismarckShuffle <107375341+BismarckShuffle@users.noreply.github.com>
2026-06-30 09:13:16 -0400

    Added Vulcanized Rubber Strips item, recipe, and registration
    Signed-off-by: BismarckShuffle <107375341+BismarckShuffle@users.noreply.github.com>

--- src/main/java/com/bismarckshuffle/createvulcanized/registry/AllItems.java
+++ src/main/java/com/bismarckshuffle/createvulcanized/registry/AllItems.java
@@ -1,7 +1,9 @@
 package com.bismarckshuffle.createvulcanized.registry;
 
 import com.bismarckshuffle.createvulcanized.items.RubberSheetItem;
+
 import com.tterrag.registrate.util.entry.ItemEntry;
+import net.minecraft.world.item.Item;
 
 import static com.bismarckshuffle.createvulcanized.CreateVulcanized.REGISTRATE;
 
@@ -14,6 +16,13 @@ public class AllItems {
             REGISTRATE.item("raw_rubber_sheet", RubberSheetItem::new)
                     .properties(p -> p.stacksTo(64))
                     .lang("Raw Rubber Sheet")
+                    .register();
+
+    @SuppressWarnings("unused")
+    public static final ItemEntry<Item> VULCANIZED_RUBBER_STRIPS =
+            REGISTRATE.item("vulcanized_rubber_strips", Item::new)
+                    .properties(p -> p.stacksTo(64))
+                    .lang("Vulcanized Rubber Strips")
                     .register();
 
 
