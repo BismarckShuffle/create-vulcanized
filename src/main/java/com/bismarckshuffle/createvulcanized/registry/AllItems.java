3ad936443243b7d107cfe1eddec2c385e9fa83ee
BismarckShuffle <107375341+BismarckShuffle@users.noreply.github.com>
2026-07-02 14:58:18 -0400

    feat: add andesite fastener item with updated properties
    fix: moved several item names to not override lang files for translation
    Signed-off-by: BismarckShuffle <107375341+BismarckShuffle@users.noreply.github.com>

--- src/main/java/com/bismarckshuffle/createvulcanized/registry/AllItems.java
+++ src/main/java/com/bismarckshuffle/createvulcanized/registry/AllItems.java
@@ -15,16 +15,19 @@ public class AllItems {
     public static final ItemEntry<RubberSheetItem> RAW_RUBBER_SHEET =
             REGISTRATE.item("raw_rubber_sheet", RubberSheetItem::new)
                     .properties(p -> p.stacksTo(64))
-                    .lang("Raw Rubber Sheet")
                     .register();
 
     @SuppressWarnings("unused")
     public static final ItemEntry<Item> VULCANIZED_RUBBER_STRIPS =
             REGISTRATE.item("vulcanized_rubber_strips", Item::new)
-                    .properties(p -> p.stacksTo(64))
-                    .lang("Vulcanized Rubber Strips")
+                    .properties(p -> p.stacksTo(32))
                     .register();
 
+    @SuppressWarnings("unused")
+    public static final ItemEntry<Item> ANDESITE_FASTENER =
+            REGISTRATE.item("andesite_fastener", Item::new)
+                    .properties(p -> p.stacksTo(32))
+                    .register();
 
     public static void register() {
         // Force class loading to trigger Registrate calls
